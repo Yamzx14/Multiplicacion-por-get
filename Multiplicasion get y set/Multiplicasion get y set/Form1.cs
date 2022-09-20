43e302fd57a00caa89618a2fc8ae367794dec4da
Yamzx14 <99759853+Yamzx14@users.noreply.github.com>
2022-09-19 22:31:36 -0500

    Add files via upload

---
+++ Multiplicasion get y set/Multiplicasion get y set/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplicasion_get_y_set
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            multiplicaclass mul = new multiplicaclass();
+            mul.N11=int.Parse(textBox1.Text);
+            mul.N21=int.Parse(textBox2.Text);
+            textBox3.Text = mul.multiplica().ToString();
+        }
+    }
+}
---
+++ Multiplicasion get y set/Multiplicasion get y set/multi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicasion_get_y_set
+{
+    internal class multiplicaclass
+    {
+      
+        private int Nu1;
+        private int Nu2;
+
+        public int N11 { get => Nu1; set => Nu1 = value; }
+        public int N21 { get => Nu2; set => Nu2 = value; }
+
+        public int multiplica()
+        {
+            return (Nu1 * Nu2);
+        }
+    }
+}
